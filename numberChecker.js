9c252161e9e87b0634985fd38f2274db5c5a44f6
Hidayat <89620222+hidayatrpl@users.noreply.github.com>
2026-08-19 19:45:01 +0700

    add done task

---
+++ numberChecker.js
@@ -0,0 +1,32 @@
+const isPositive = (number) => {
+    return number > 0;
+}
+const isNegative = (number) => {
+    return number < 0;
+}
+const isZero = (number) => {
+    return number === 0;
+}
+const isEven = (number) => {
+    if (number % 2 === 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+const describeNumber = (number) => {
+    return {
+        positive: isPositive(number),
+        negative: isNegative(number),
+        zero: isZero(number),
+        even: isEven(number),
+        odd: !isEven(number)
+    };
+};
+
+
+console.log(describeNumber(8));
+console.log(describeNumber(-3));
+console.log(describeNumber(0));
+console.log(describeNumber(7));
